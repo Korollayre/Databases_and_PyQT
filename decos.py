3a460340ba132ad0630bcb02334b57e2961457ca
Mikhail <mbuinichienko@mail.ru>
2021-11-22 11:24:05 +0300

    Add final CS_App_Python files

---
+++ decos.py
@@ -0,0 +1,25 @@
+import logging
+import traceback
+import sys
+import inspect
+
+import logs.server_log_config
+import logs.client_log_config
+
+if sys.argv[0].find('client') == -1:
+    LOGGER = logging.getLogger('server')
+else:
+    LOGGER = logging.getLogger('client')
+
+
+class Log:
+    def __call__(self, func):
+        def decorated(*args, **kwargs):
+            splitter = '\\'
+            res = func(*args, **kwargs)
+            LOGGER.info(f'Произошел вызов функции {func.__name__} со следующими параметрами - {args}, {kwargs}. '
+                        f'Вызов произошел из функции {traceback.format_stack()[0].split()[-1]} '
+                        f'модуля {inspect.stack()[0][1].split(splitter)[-1]}')
+            return res
+
+        return decorated
---
+++ tests/test_server.py
@@ -0,0 +1,33 @@
+import unittest
+
+from common.variables import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE, PORT, \
+    RESPONDEFAULT_IP_ADDRESSSE
+from client import user_request
+from server import process_user_message
+
+
+class TestClient(unittest.TestCase):
+    error_response = {
+        RESPONDEFAULT_IP_ADDRESSSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+    correct_response = {RESPONSE: 200}
+
+    # Equal - Корректный запрос
+    def test_presense(self):
+        self.assertEqual(process_user_message(user_request(8888)), self.correct_response)
+
+    # NoEqual - некорректный запрос, так как отсутствует порт
+    def test_incorrect_presense(self):
+        self.assertNotEqual(process_user_message({ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}),
+                            self.correct_response)
+
+    # Equal - Корректный запрос, так как отсутствует порт
+    def test_correct_incorrect_presense(self):
+        self.assertEqual(process_user_message({ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}),
+                         self.error_response)
+
+
+if __name__ == '__main__':
+    unittest.main()
