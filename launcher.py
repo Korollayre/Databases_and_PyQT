f7b6ca7999b9903606f14d310e40aa38e8c6b287
Mikhail <mbuinichienko@mail.ru>
2021-12-27 11:42:36 +0300

    Added docstrings and separate command to run server and client

--- launcher.py
+++ launcher.py
@@ -1,26 +1,32 @@
+"""
+Модуль для запуска серверной и клиентской части
+"""
 import subprocess
 
 
 def main():
     process = []
     venv_path = '../venv/Scripts/python'
+    clients_online = False
     while True:
-        ACTION = input("Для запуска сервера и клиентов введите 's', для выхода - 'q', для закрытия всех окон 'x': ")
+        command = input("Для запуска сервера и клиентов введите 's', для выхода - 'q', для закрытия всех окон 'x': ")
 
-        if ACTION == 'q':
+        if command == 'q':
             break
-        elif ACTION == 's':
-            # users = int(input('Введите количество клиентов для запуска: '))
-
+        elif command == 's':
             process.append(
                 subprocess.Popen(f'{venv_path} server.py', creationflags=subprocess.CREATE_NO_WINDOW))
+        elif command == 'k':
+            if not clients_online:
+                for i in range(3):
+                    process.append(
+                        subprocess.Popen(f'{venv_path} client.py -n test{i + 1} -p password',
+                                         creationflags=subprocess.CREATE_NO_WINDOW))
+                    clients_online = True
+            else:
+                print('Клиенты уже запущены.')
 
-            for i in range(3):
-                process.append(
-                    subprocess.Popen(f'{venv_path} client.py -n test{i + 1} -p password',
-                                     creationflags=subprocess.CREATE_NO_WINDOW))
-
-        elif ACTION == 'x':
+        elif command == 'x':
             while process:
                 victim = process.pop()
                 victim.kill()
