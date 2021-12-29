e1ca75ce4036e91ecbb341b828c8ec8a5ecf154c
Mikhail <mbuinichienko@mail.ru>
2021-12-29 22:34:19 +0300

    Did client_setup.py file with settings to make exe file for client module

--- client/client_setup.py
+++ client/client_setup.py
@@ -2,17 +2,17 @@ import sys
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
-    "packages": ["common", "logs", "client"],
+    'packages': ['common', 'logs', 'client', 'sqlalchemy', 'sqlite3'],
 }
 
-setup(name="Byunichenko_DB_and_PyQt_client_module",
-      version="0.0.1",
-      description="message_client_app",
+setup(name='Byunichenko_DB_and_PyQt_client_module',
+      version='0.0.1',
+      description='message_client_app',
       options={
-          "build_exe": build_exe_options
+          'build_exe': build_exe_options
       },
       executables=[Executable('client.py',
                               base='Win32GUI',
-                              targetName='client.exe',
+                              target_name='client.exe',
                               )]
       )
