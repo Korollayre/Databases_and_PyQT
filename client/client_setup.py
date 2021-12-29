cb761dbe11d740a0a197a1eb6b86c65c41100788
Mikhail <mbuinichienko@mail.ru>
2021-12-29 21:39:18 +0300

    Did client_setup.py file with settings for cx_Freeze

---
+++ client/client_setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages": ["common", "logs", "client"],
+}
+
+setup(name="Byunichenko_DB_and_PyQt_client_module",
+      version="0.0.1",
+      description="message_client_app",
+      options={
+          "build_exe": build_exe_options
+      },
+      executables=[Executable('client.py',
+                              base='Win32GUI',
+                              targetName='client.exe',
+                              )]
+      )
